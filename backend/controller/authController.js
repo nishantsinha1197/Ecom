d02ac6d3c5708b45add3f1925afe55465979eff4
nishantsinha1197 <nishantsinha1197@gmail.com>
2024-02-08 00:26:20 +0530

    Created routes for registration ,login and also protected routes for admin isRequire and isAdmin

---
+++ backend/controller/authController.js
@@ -0,0 +1,104 @@
+import { encryptPassword, matchPassword } from "../helper/authHelper.js";
+import usersModel from "../model/usersModel.js";
+import jwt from "jsonwebtoken";
+//controller for registration
+export let registerController = async (req, res) => {
+  let { email, password, name, address, phone } = req.body;
+  try {
+    if (!email) {
+      return res.status(500).send({ message: "email is required *" });
+    }
+    if (!password) {
+      return res.status(500).send({ message: "password is required *" });
+    }
+    if (!name) {
+      return res.status(500).send({ message: "name is required *" });
+    }
+    if (!address) {
+      return res.status(500).send({ message: "address is required *" });
+    }
+    if (!phone) {
+      return res.status(500).send({ message: "phone is required *" });
+    }
+    let findUser = await usersModel.findOne({ email: email });
+    if (findUser) {
+      return res.status(200).send({ message: "User is Already Registered" });
+    }
+    let hashpassword = await encryptPassword(password);
+    let user = await new usersModel({
+      name,
+      password: hashpassword,
+      address,
+      phone,
+      email,
+    }).save();
+
+    res
+      .status(201)
+      .send({ message: "User is registered Successfully", success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Somthing wrong while registration",
+      err,
+      success: false,
+    });
+  }
+};
+
+export let loginController = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    // console.log(req.body);
+    // res.send('hello')
+
+    //Valiadtion for email and password
+    if (!email) {
+      res.status(500).send({ message: "Email is required" });
+    }
+    if (!password) {
+      res.status(500).send({ message: "Password is required" });
+    }
+    let existingUser = await usersModel.findOne({ email: email });
+
+    if (!existingUser) {
+      return res
+        .status(200)
+        .send({ message: "Either email or password is invalid" });
+    }
+
+    let result = await matchPassword(password, existingUser.password);
+    if (!result) {
+      return res
+        .status(200)
+        .send({ message: "Either email or password is invalid" });
+    }
+    //Token creation when user is logged in successfully
+    let token = await jwt.sign(
+      { _id: existingUser._id },
+      process.env.SCERET_KEY,
+      { expiresIn: "7d" }
+    );
+    console.log('token',token);
+    res.status(200).send({
+      message:'User logged in successfully',
+      success:true,
+      user : {
+        name:existingUser.name,
+        email:existingUser.email,
+        phone:existingUser.phone,
+        role:existingUser.role,
+        address:existingUser.address
+      },
+      token
+    })
+  } catch (err) {
+    res
+      .status(200)
+      .send({
+        message: "Something went wrong while trying to log in",
+        success: "false",
+        err,
+      });
+  }
+};
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import usersModel from "../model/usersModel.js";
+import jwt from "jsonwebtoken";
+
+export let isRequire = async (req, res, next) => {
+    try {
+        let decode = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+        req.user = decode;
+        console.log('Hi I am decode', decode);
+        if (!decode) {
+            res.status(200).send({ message: "Unauthorized User" });
+        }
+        next();
+    } catch (err) {
+        console.log(err);
+        res.status(200).send({ message: 'User is not authorized' });
+    }
+};
+
+// For admin
+
+export let isAdmin = async (req, res, next) => {
+    let userData = await usersModel.findById({ _id: req.user._id });
+    if (userData.role != 0) {
+        next();
+    } else {
+        res.status(200).send({ message: 'User is not authorized' });
+    }
+};
